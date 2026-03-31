e3087baaf0f262572f8a723d880c1ea4ca287b9c
codenicollas <work.nicollas@gmail.com>
2026-03-31 20:20:40 -0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
+import modelos.Data;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite seu nome: ");
         String nome = scanner.nextLine();
-
-        
     }
 }
