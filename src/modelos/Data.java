c12a5cfecff336627cc60c51d6e2033285d90b15
finnyfang <finnyomega@gmail.com>
2026-03-31 20:17:10 -0300

    changes

--- src/modelos/Data.java
+++ src/modelos/Data.java
@@ -1,4 +1,15 @@
 package modelos;
 
 public class Data {
+
+    private int dia;
+    private int mes;
+    private int dataNascimento;
+
+    @Override
+    public String toString(){
+        return ""+this.dia + this.mes + this.dataNascimento;
+
+    }
+
 }
