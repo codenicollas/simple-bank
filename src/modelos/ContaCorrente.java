d7b1194b64e1d8cad81a0a3e828b145b238b6be2
FinnyFang <142420234+FinnyFang@users.noreply.github.com>
2026-04-04 21:44:19 -0300

    changes

---
+++ src/modelos/ContaCorrente.java
@@ -0,0 +1,11 @@
+package modelos;
+
+public class ContaCorrente extends ContaBancaria{
+
+    private double limiteCredito;
+
+    public void movimentaLimiteCredito(double limiteCredito){
+        // A ESCREVER
+    }
+
+}
